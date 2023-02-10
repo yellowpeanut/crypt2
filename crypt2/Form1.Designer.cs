84f9756d24ec0d0b5daa3d3c5b915b7a9ed52365
User <yellowpeanut42@rambler.ru>
2023-02-10 15:34:07 +0300

    Добавьте файлы проекта.

---
+++ crypt2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace crypt2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Initial file";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(334, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Decompressed file";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Compress";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(95, 337);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Decompress";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Compressed file";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(334, 354);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(300, 44);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 410);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ crypt2/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crypt2
+{
+    public partial class Form1 : Form
+    {
+        private string fileContent = String.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            generatePicture(10000);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "txt files (*.txt)|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileStream = dialog.OpenFile();
+                fileContent = File.ReadAllText(dialog.FileName);
+                if (fileContent.Count() == 10000)
+                    setPicture(pictureBox1, fileContent);
+                else
+                {
+                    MessageBox.Show($"File does not contain 10000 symbols! (Symbol count - {fileContent.Count()})", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error opening the file", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string compressedFile = compressFile(fileContent);
+            richTextBox1.Text = compressedFile;
+            File.WriteAllText("2.txt", compressedFile);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string compressedFile = File.ReadAllText("2.txt");
+            string decompressedFile = decompressFile(compressedFile);
+            File.WriteAllText("3.txt", decompressedFile);
+            setPicture(pictureBox2, decompressedFile);
+        }
+
+        private void setPicture(PictureBox pb, string file)
+        {
+            var bitmap = new Bitmap(100, 100);
+            for (int x = 0; x < 100; x++)
+            {
+                for (int y = 0; y < 100; y++)
+                {
+                    if (file[x * 100 + y] == '0')
+                        bitmap.SetPixel(x, y, Color.White);
+                    else if (file[x * 100 + y] == '1')
+                        bitmap.SetPixel(x, y, Color.Black);
+                    else
+                        MessageBox.Show("File contains unallowed symbols! Only symbols '1' and '0' are allowed", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            pb.Image = bitmap;
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private string compressFile(string file)
+        {
+            string compressedFile = String.Empty;
+            int numberOfMatches = 0;
+            for (int i = 0; i < file.Length / 8; i++)
+            {
+                int amount = 1;
+                string currByte = file.Substring(i * 8, 8);
+                while (amount < 64 && i + 1 < file.Length / 8)
+                {
+                    string nextByte = file.Substring((i + 1) * 8, 8);
+                    if (currByte == nextByte)
+                    {
+                        i++; amount++;
+                    }
+                    else
+                    {
+                        numberOfMatches += amount-1;
+                        break;
+                    }
+                }
+                string counter = "11" + Format(Convert.ToString(amount, 2));
+                compressedFile += (counter + currByte);
+            }
+            MessageBox.Show($"Success!\nAmount of matching bytes: {numberOfMatches}");
+            return compressedFile.ToString();
+        }
+        private string decompressFile(string file)
+        {
+            string decompressedFile = String.Empty;
+            int amount = 0;
+            string currByte = String.Empty;
+            int len = file.Length;
+            for (int i = 0; i < file.Length; i++)
+            {
+                amount = Convert.ToInt32(file.Substring(i + 2, 6), 2);
+                i += 8;
+                currByte = file.Substring(i, 8);
+                i += 7;
+                for (int j = 0; j < amount; j++)
+                {
+                    decompressedFile += currByte;
+                }
+            }
+            return decompressedFile;
+        }
+
+        private string Format(string str)
+        {
+            string newStr = String.Empty;
+            for (int i = 0; i < 6 - str.Length; i++)
+            {
+                newStr += "0";
+            }
+            newStr += str;
+            return newStr;
+        }
+
+        private void generatePicture(int length)
+        {
+            string text = String.Empty;
+            Random rand = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                text += rand.Next(0, 2).ToString();
+            }
+            File.WriteAllText("1.txt", text);
+        }
+    }
+}
